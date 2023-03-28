e2fc63f6c237890699a336fd9fb80d88e8aa1265
Ikeoluwapo <eikeoluwapo@gmail.com>
2023-03-28 01:46:22 -0700

    push

--- main.h
+++ main.h
@@ -9,3 +9,21 @@ int _putchar (char c);
 int print_percent(va_list c);
 int print_char(va_list arguments);
 int print_string(va_list arguments);
+int print_binary(va_list);
+
+/**
+ * struct top - structure
+ * @c: character
+ * @p: pointer
+ * Return: 0
+ */
+
+struct top
+{
+	char *c;
+	int (*p)(va_list);
+};
+typedef struct top var_a;
+
+
+#endif /* main.h */
