be8d096002a29b160b012b665921907f6b507a37
OluwatoyosiAbolaji <oluwatoyosi.abolaji25@gmail.com>
2023-03-28 10:11:40 +0100

    print strings function

---
+++ print_strings.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+
+/**
+ * print_char - print characters
+ * @arguments: functions
+ *
+ * Return: 0
+ */
+
+int print_char(va_list arguments)
+{
+	char variable = va_arg(arguments, int);
+
+	_putchar(variable);
+	return (1);
+}
+
+/**
+ * print_string - print string
+ * @arguments: functions
+ *
+ * Return: 0
+ */
+
+int print_string(va_list arguments)
+{
+	int i, len = 0;
+
+	char *str;
+
+	str = va_arg(arguments, char *);
+	if (str == NULL)
+	{
+		return (0);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+		len +=  _putchar(str[i]);
+	return (len);
+}
+
+/**
+ * print_percent - print string
+ * @c: functions
+ *
+ * Return: 0
+ */
+
+//int print_percent(va_list c)
+{
+	_putchar (37);
+	return (1);
+}
